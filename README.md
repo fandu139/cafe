31f9bffd30e6a84e64c390a0a535f309565e251d
eezhal92 <arkinthesky.69@gmail.com>
2017-11-28 12:03:13 +0800

    fix code styling. update dokumentasi

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-"# cafe-reservation" 
+# cafe-reservation
+
+## Kebutuhan
+- Node 8.9+
+
+## Cara setup local
+- Install semua dependensi package `npm install`
+- Jalankan aplikasi `npm run dev`
--- app.js
+++ app.js
@@ -1,50 +1,49 @@
 'use strict'
 
 var express = require('express');
-var app = module.exports = express();
+var app = express();
 var http = require('http');
 var routers = require('./routes/index');
 
+/**
+ * Configs
+ */
+
 app.set('port',process.env.PORT || 3030); // PORT
 app.set('views', __dirname); // set folder views
-app.use(express.static(__dirname + '/public')); // set folder css dan javascript
 app.set('view engine', 'ejs'); // set enggine tampilan ejs
 
+/**
+ * Middlewares
+ */
+
+app.use(express.static(__dirname + '/public')); // set folder css dan javascript
+
+/**
+ * Routes
+ */
+
 app.get('/',function(req,res){   // root awal web 
-    res.render(__dirname + '/views/index.ejs');
+  res.render(__dirname + '/views/index.ejs');
 });
-
-/*app.use(function(req,res,fn){ // halaman tidak di temukan
-  res.render('error_page', {status:404,url:req.url,error: 'Maaf Halaman Tidak Di Temukan'});
-});*/
-
-
-/*var welcome = require('./javascript/welcome');
-
-app.use(welcome);*/
 
 app.get('/welcome',function(req,res){   // root awal web 
-    res.render(__dirname + '/views/welcome.ejs');
+  res.render(__dirname + '/views/welcome.ejs');
 });
 
-// Handle 404
-app.use(function(req, res) {
-    res.send('404: Page not Found', 404);
-});
   
-// Handle 500
-app.use(function(error, req, res, next) {
-	res.send('500: Internal Server Error', 500);
+/**
+ * Error Handlers
+ */
+
+app.use(function(req, res) {
+  res.send('404: Page not Found', 404);
 });
 
-/*app.use(function(err, req, res, next){ //error page
-  res.render('error_page', {
-    status: err.status || 500,
-    error: err
-  });
-});*/
+app.use(function(error, req, res, next) {
+	res.send(error, 500);
+});
 
-// Jalankan Server
 http.createServer(app).listen(app.get('port'),function(){
-    console.log('Server anda jalan menggunakan Port : %s ', app.get('port'));
+  console.log('Server Anda jalan menggunakan Port : %s ', app.get('port'));
 });
